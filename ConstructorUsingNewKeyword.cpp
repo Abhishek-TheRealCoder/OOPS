efe5d230664b6516b6299ea338e4b673a3f394a4
Abhishek-TheRealCoder <abhisheksingh11102003@gmail.com>
2025-07-12 23:21:42 +0530

    Add example of constructor call with new/delete for Student class

---
+++ ConstructorUsingNewKeyword.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+    public:
+        string name;
+        int age;
+        string subject_stream;
+        
+        //Default constructor
+        Student(){
+            cout<<"this is default constructor"<<endl;
+        }   
+        Student(string name,int age,string subject_stream){
+            this->name = name;
+            this->age = age;
+            this->subject_stream = subject_stream;
+        }
+
+        void info(){
+            cout<<"name : "<<name<<endl;
+            cout<<"age : "<<age<<endl;
+            cout<<"subject_stream : "<<subject_stream<<endl;
+        }
+};
+int main(){
+    Student* ptr = new Student("Abhishek Singh",23,"Computer Science");
+    ptr->info();
+    return 0;
+}
+
+// Stack Memory:                        Heap Memory:
+// +---------------------+              +---------------------------------------------+
+// | ptr (shared_ptr)    | ---------->  | Student object                             |
+// +---------------------+              |---------------------------------------------|
+//                                      | name           = "Abhishek Singh"          |
+//                                      | age            = 23                        |
+//                                      | subject_stream = "Computer Science"        |
+//                                      +---------------------------------------------+
